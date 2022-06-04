91791e21f89d160bb080d21f201640733c01373e
winterandrea <andrecasero@gmail.com>
2022-06-04 12:04:19 +0200

    readme

--- src/contract/callContract.js
+++ src/contract/callContract.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import { abi, address } from "../contract/contract";
 
-const url = "https://polygon-mainnet.g.alchemy.com/v2/PTORvcN4KFP437RSKVc2NoPl1ohgI4re";
+const url = `https://polygon-mainnet.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY}`;
 const provider = new ethers.providers.JsonRpcProvider(url);
 export const contract = new ethers.Contract(address, abi, provider);
