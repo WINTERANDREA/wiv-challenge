111278926977a65154a0062b624b798b05d16c3f
winterandrea <andrecasero@gmail.com>
2022-06-04 09:44:34 +0200

    marketplace

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import Page from "v-page";
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +9,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Page);
 
 app.mount("#app");
