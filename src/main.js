0d763ecf5f0755b27840a98d31322519349c9ae2
winterandrea <andrecasero@gmail.com>
2022-06-04 09:46:00 +0200

    main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import Page from "v-page";
-
 import App from "./App.vue";
 import router from "./router";
 
@@ -9,6 +7,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(Page);
 
 app.mount("#app");
